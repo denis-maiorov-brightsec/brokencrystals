dfd2b4c2e72cd6ca912f9060104e724789770a5b
denis-maiorov-brightsec <103932492+denis-maiorov-brightsec@users.noreply.github.com>
2026-05-15 15:49:01 +0200

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-api-metadata.test.ts
@@ -0,0 +1,41 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /api/metadata', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['xxe'],
+      attackParamLocations: [AttackParamLocation.BODY],
+      starMetadata: {
+        code_source: 'denis-maiorov-brightsec/brokencrystals:stable',
+        databases: ['PostgreSQL'],
+        user_roles: []
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/api/metadata`,
+      body: `<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE foo [<!ELEMENT foo ANY ><!ENTITY xxe SYSTEM "file:///etc/passwd" >]>\n<foo>&xxe;</foo>`,
+      headers: { 'Content-Type': 'text/xml' }
+    });
+});
