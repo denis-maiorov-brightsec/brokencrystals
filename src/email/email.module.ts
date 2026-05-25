a6e3c20908c496c9a746e022172297a966b178c7
denis-maiorov-brightsec <103932492+denis-maiorov-brightsec@users.noreply.github.com>
2026-05-25 16:53:17 +0200

    ci: persist CI Agent runtime patch
    skip-checks:true

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { EmailController } from './email.controller';
 import { EmailService } from './email.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [EmailController],
   providers: [EmailService]
 })
